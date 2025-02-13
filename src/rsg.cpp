1760ffc32a94c0a6a370f8adc8a86badadc06c41
beliumgl <beliumgl@proton.me>
2025-02-13 22:43:04 +0500

    сменена инициализация переменных с = на {}

--- src/rsg.cpp
+++ src/rsg.cpp
@@ -9,8 +9,8 @@ namespace RSG {
     std::string RSG::Generate() {
         std::string result;
 
-        std::map<std::string, bool> requires = this->rules.GetRequires();
-        std::map<std::string, std::optional<unsigned short>> rules = this->rules.GetRules();
+        std::map<std::string, bool> requires{ this->rules.GetRequires() };
+        std::map<std::string, std::optional<unsigned short>> rules{ this->rules.GetRules() };
         std::optional<Charset> charset;
         std::random_device rd;
 
@@ -32,59 +32,59 @@ namespace RSG {
             charset = Charset(this->charset);
         }
 
-        for (size_t i = 0; i < length; ++i) {
+        for (size_t i{ 0 }; i < length; ++i) {
             GENERATE_CHAR:
-            std::uniform_int_distribution<size_t> dist(0, this->charset.size());
-            char randomChar = this->charset[dist(rd)];
+                std::uniform_int_distribution<size_t> dist(0, this->charset.size());
+                char randomChar{ this->charset[dist(rd)] };
 
-            if (randomChar == '\0') {
-                goto GENERATE_CHAR;
-            }
-            if (requires.at("MAX_REPEAT_OF_THE_SAME_CHARACTER_CATEGORY_REQUIRED") &&
-                usedCategory.has_value() && charset->GetCharset(randomChar) == usedCategory.value() &&
-                repeatedSameCategoryCount.has_value() && repeatedSameCategoryCount.value() >= rules.at("MAX_REPEAT_OF_THE_SAME_CHARACTER_CATEGORY").value())
-            {
-                goto GENERATE_CHAR;
-            }
-            else if (requires.at("MAX_REPEAT_OF_THE_SAME_CHARACTER_REQUIRED") &&
-                     usedCharacter.has_value() && randomChar == usedCharacter.value() &&
-                     repeatedCharacterCount.has_value() && repeatedCharacterCount.value() >= rules.at("MAX_REPEAT_OF_THE_SAME_CHARACTER").value())
-            {
-                goto GENERATE_CHAR;
-            }
-            else if (requires.at("MAX_REPEAT_OF_THE_SAME_CHARACTER_IN_DIFFERENT_CATEGORIES_REQUIRED") &&
-                     usedCharacter.has_value() && std::tolower(randomChar) == std::tolower(usedCharacter.value()) &&
-                     repeatedCharacterInTheDifferentCategoriesCount.has_value() && repeatedCharacterInTheDifferentCategoriesCount.value() >= rules.at("MAX_REPEAT_OF_THE_SAME_CHARACTER_IN_DIFFERENT_CATEGORIES").value())
-            {
-                goto GENERATE_CHAR;
-            }
-            result.push_back(randomChar);
-            if (requires.at("MAX_REPEAT_OF_THE_SAME_CHARACTER_CATEGORY_REQUIRED")) {
-                if (usedCategory.has_value() && charset->GetCharset(randomChar) == usedCategory.value()) {
-                    ++repeatedSameCategoryCount.value();
+                if (randomChar == '\0') {
+                    goto GENERATE_CHAR;
                 }
-                else {
-                    repeatedSameCategoryCount = 0;
+                if (requires.at("MAX_REPEAT_OF_THE_SAME_CHARACTER_CATEGORY_REQUIRED") &&
+                    usedCategory.has_value() && charset->GetCharset(randomChar) == usedCategory.value() &&
+                    repeatedSameCategoryCount.has_value() && repeatedSameCategoryCount.value() >= rules.at("MAX_REPEAT_OF_THE_SAME_CHARACTER_CATEGORY").value())
+                {
+                    goto GENERATE_CHAR;
                 }
-            }
-            if (requires.at("MAX_REPEAT_OF_THE_SAME_CHARACTER_REQUIRED")) {
-                if (usedCharacter.has_value() && randomChar == usedCharacter.value()) {
-                    ++repeatedCharacterCount.value();
+                else if (requires.at("MAX_REPEAT_OF_THE_SAME_CHARACTER_REQUIRED") &&
+                        usedCharacter.has_value() && randomChar == usedCharacter.value() &&
+                        repeatedCharacterCount.has_value() && repeatedCharacterCount.value() >= rules.at("MAX_REPEAT_OF_THE_SAME_CHARACTER").value())
+                {
+                    goto GENERATE_CHAR;
                 }
-                else {
-                    repeatedCharacterCount = 0;
+                else if (requires.at("MAX_REPEAT_OF_THE_SAME_CHARACTER_IN_DIFFERENT_CATEGORIES_REQUIRED") &&
+                        usedCharacter.has_value() && std::tolower(randomChar) == std::tolower(usedCharacter.value()) &&
+                        repeatedCharacterInTheDifferentCategoriesCount.has_value() && repeatedCharacterInTheDifferentCategoriesCount.value() >= rules.at("MAX_REPEAT_OF_THE_SAME_CHARACTER_IN_DIFFERENT_CATEGORIES").value())
+                {
+                    goto GENERATE_CHAR;
                 }
-            }
-            if (requires.at("MAX_REPEAT_OF_THE_SAME_CHARACTER_IN_DIFFERENT_CATEGORIES_REQUIRED")) {
-                if (usedCharacter.has_value() && std::tolower(randomChar) == std::tolower(usedCharacter.value())) {
-                    ++repeatedCharacterInTheDifferentCategoriesCount.value();
+                result.push_back(randomChar);
+                if (requires.at("MAX_REPEAT_OF_THE_SAME_CHARACTER_CATEGORY_REQUIRED")) {
+                    if (usedCategory.has_value() && charset->GetCharset(randomChar) == usedCategory.value()) {
+                        ++repeatedSameCategoryCount.value();
+                    }
+                    else {
+                        repeatedSameCategoryCount = 0;
+                    }
                 }
-                else {
-                    repeatedCharacterInTheDifferentCategoriesCount.value() = 0;
+                if (requires.at("MAX_REPEAT_OF_THE_SAME_CHARACTER_REQUIRED")) {
+                    if (usedCharacter.has_value() && randomChar == usedCharacter.value()) {
+                        ++repeatedCharacterCount.value();
+                    }
+                    else {
+                        repeatedCharacterCount = 0;
+                    }
                 }
-            }
-            usedCategory = charset->GetCharset(randomChar);
-            usedCharacter = randomChar;
+                if (requires.at("MAX_REPEAT_OF_THE_SAME_CHARACTER_IN_DIFFERENT_CATEGORIES_REQUIRED")) {
+                    if (usedCharacter.has_value() && std::tolower(randomChar) == std::tolower(usedCharacter.value())) {
+                        ++repeatedCharacterInTheDifferentCategoriesCount.value();
+                    }
+                    else {
+                        repeatedCharacterInTheDifferentCategoriesCount.value() = 0;
+                    }
+                }
+                usedCategory = charset->GetCharset(randomChar);
+                usedCharacter = randomChar;
         }
 
         return result;
