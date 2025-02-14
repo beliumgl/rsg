8ec2a8f3e89976a6661d05a3ba758b80592f0612
beliumgl <beliumgl@proton.me>
2025-02-14 21:51:38 +0500

    исправлены ссылки на реальные файлы

--- testapp/include/rsg.hpp
+++ testapp/include/rsg.hpp
@@ -1,1 +1,32 @@
-/home/belium/CLionProjects/RandomStringGenerator/include/rsg.hpp
+#pragma once
+
+#include <string>
+#include <string_view>
+
+#include <include/rules.hpp>
+
+namespace RSG {
+    class RSG {
+    private:
+        std::string_view charset;
+        unsigned short length;
+        Rules rules;
+    public:
+        /*
+         * Constructor for the Random String Generator (RSG).
+         *
+         * Parameters:
+         *  - charset: any valid string_view.
+         *  - length: the desired length of the generated string.
+         *  - mode:
+         *      - mode 0: generates a truly random string.
+         *      - mode 1: generates a more secure string with additional checks.
+         *      - mode 2: generates a highly secure string.
+         */
+
+        RSG(const std::string_view& charset, unsigned short length, unsigned char mode)
+            : charset(charset), length(length), rules(Rules(mode)) {}
+
+        std::string Generate();
+    };
+}
--- testapp/include/rules.hpp
+++ testapp/include/rules.hpp
@@ -1,1 +1,39 @@
-/home/belium/CLionProjects/RandomStringGenerator/include/rules.hpp
+#pragma once
+
+#include <array>
+#include <map>
+#include <optional>
+
+namespace RSG {
+    constexpr unsigned char RULES_COUNT = 3;
+    constexpr unsigned char REQUIRES_COUNT = 3;
+    constexpr unsigned char MODES_COUNT = 3;
+
+    constexpr std::array<unsigned char, MODES_COUNT> ALLOWED_MODES {0, 1, 2};
+
+    constexpr std::array<bool, MODES_COUNT> MAX_REPEAT_OF_THE_SAME_CHARACTER_CATEGORY_REQUIRED_VALUES {false, true, true};
+    constexpr std::array<bool, MODES_COUNT> MAX_REPEAT_OF_THE_SAME_CHARACTER_REQUIRED_VALUES {false, true, true};
+    constexpr std::array<bool, MODES_COUNT> MAX_REPEAT_OF_THE_SAME_CHARACTER_IN_DIFFERENT_CATEGORIES_REQUIRED_VALUES {false, false, true};
+
+    constexpr std::array<unsigned char, MODES_COUNT> MAX_REPEAT_OF_THE_SAME_CHARACTER_CATEGORY_VALUES {NULL, 5, 3};
+    constexpr std::array<unsigned char, MODES_COUNT> MAX_REPEAT_OF_THE_SAME_CHARACTER_VALUES {NULL, 3, 1};
+    constexpr std::array<unsigned char, MODES_COUNT> MAX_REPEAT_OF_THE_SAME_CHARACTER_IN_DIFFERENT_CATEGORIES_VALUES {NULL, NULL, 3};
+
+    class Rules {
+    private:
+        // If the *_REQUIRED variable is set to `false`, then this rule will have no effect
+        bool MAX_REPEAT_OF_THE_SAME_CHARACTER_CATEGORY_REQUIRED = false;
+        bool MAX_REPEAT_OF_THE_SAME_CHARACTER_REQUIRED = false;
+        bool MAX_REPEAT_OF_THE_SAME_CHARACTER_IN_DIFFERENT_CATEGORIES_REQUIRED = false;
+        std::optional<unsigned short> MAX_REPEAT_OF_THE_SAME_CHARACTER_CATEGORY;                        // This variable specifies the maximum number of consecutive characters allowed from the same category (e.g., AAAAA, aaaaa, @@@@@).
+        std::optional<unsigned short> MAX_REPEAT_OF_THE_SAME_CHARACTER;                                 // This variable specifies the maximum number of consecutive occurrences of the same character (e.g., aaaaa, bbbbb, !!!!!).
+        std::optional<unsigned short> MAX_REPEAT_OF_THE_SAME_CHARACTER_IN_DIFFERENT_CATEGORIES;         // This variable specifies the maximum number of occurrences of the same character across different categories (e.g., AaaAa, BbbBBb, oOoOO).
+    public:
+        Rules(unsigned char mode);
+
+        // Getters
+        std::map<std::string, bool> GetRequires();                           // Return all *_REQUIRED variables
+        std::map<std::string, std::optional<unsigned short>> GetRules();     // Return all rule variables
+    };
+}
+
